7ea9343017553ed8a83da7176b3027bc8f485b48
Kristoffer1122 <krkja001@osloskolen.no>
2026-02-27 15:28:34 +0100

    library page

---
+++ app/gamelibrary/page.tsx
@@ -0,0 +1,12 @@
+import GameLibrary from "@/app/components/Game_Library";
+
+export default function Home() {
+
+    return (
+        <div>
+            <main>
+                <GameLibrary />
+            </main>
+        </div>
+    );
+}
