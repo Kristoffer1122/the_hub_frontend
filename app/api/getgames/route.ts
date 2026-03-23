66ad3f5eb27d3647a15778b1fef6471dd92662d4
Kristoffer1122 <krkja001@osloskolen.no>
2026-03-23 15:55:17 +0100

    better error handling

--- app/api/getgames/route.ts
+++ app/api/getgames/route.ts
@@ -1,7 +1,13 @@
 const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:7878';
 
 export async function GET() {
-    const response = await fetch(`${BACKEND_URL}/getgames`);
-    const data = await response.json();
-    return Response.json(data);
+    try {
+        const response = await fetch(`${BACKEND_URL}/getgames`);
+        const data = await response.json();
+        return Response.json(data);
+    } catch (error) {
+
+        console.log("Failed to fetch:", error);
+        return Response.json([]);
+    }
 }
